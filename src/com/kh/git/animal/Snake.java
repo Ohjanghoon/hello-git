318843411ff85779c0908927a14552de97894cd0
Ohjanghoon <100773684+Ohjanghoon@users.noreply.github.com>
2022-06-13 20:43:38 +0900

    Snake#eat 메소드 추가

--- src/com/kh/git/animal/Snake.java
+++ src/com/kh/git/animal/Snake.java
@@ -5,7 +5,11 @@ public class Snake {
 	public void move() {
 		System.out.println("스르륵~");
 	}
-	public void rung() {
+	public void run() {
 		System.out.println("Snake run~");	
+	}
+	
+	public void eat() {
+		System.out.println("냠냠");
 	}
 }
